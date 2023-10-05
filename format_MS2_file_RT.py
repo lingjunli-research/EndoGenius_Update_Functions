3832d61177c0eb77408e6b24404d9d99fde2a07e
Lauren Fields <67647952+laurenfields@users.noreply.github.com>
2023-10-05 12:51:56 -0500

    Add files via upload
    Corrected format_MS2_file_RT; old version had bug with number of lines
    
    New spectral library build app, will be integrated into EndoGenius in future version

--- format_MS2_file_RT.py
+++ format_MS2_file_RT.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  4 14:05:27 2023
+Created on Thu Oct  5 12:35:20 2023
 
 @author: lawashburn
 """
@@ -9,114 +9,104 @@ import csv
 import pandas as pd
 import os
 
-MS2_path = r"D:\EndoGenius_Updates\v1.0.3_SLbuild\pt01_new_rawfile_formatting\input\MS2_eval_short.ms2"
+MS2_path = r"C:\Users\lawashburn\Documents\EndoGeniusDistributions\version_assessment_input\fasta_raw\2021_0817_CoG_1.ms2"
 output_directory = r"D:\EndoGenius_Updates\v1.0.3_SLbuild\pt01_new_rawfile_formatting\output"
 
-######## Extracting sample name ###############
-backslash_index1 = MS2_path.rfind('\\')
-backslash_index2 = MS2_path.rfind('/')
+def format_raw_MS2(MS2_path,output_directory):
+    backslash_index1 = MS2_path.rfind('\\')
+    backslash_index2 = MS2_path.rfind('/')
 
-if backslash_index1 > backslash_index2:
-    backslash_index = backslash_index1
-elif backslash_index2 >= backslash_index1:
-    backslash_index = backslash_index2
+    if backslash_index1 > backslash_index2:
+        backslash_index = backslash_index1
+    elif backslash_index2 >= backslash_index1:
+        backslash_index = backslash_index2
 
-base_file_path = MS2_path[0:(backslash_index+1)]
+    base_file_path = MS2_path[0:(backslash_index+1)]
 
-tissue_type = MS2_path.replace(base_file_path,'')
-tissue_type = tissue_type.replace('.ms2','')
+    tissue_type = MS2_path.replace(base_file_path,'')
+    tissue_type = tissue_type.replace('.ms2','')
+    with open(MS2_path) as input:
+        lst = [line.strip() for line in input]
+    
+    new_list= []
+    final_lst = []
+    final_lst.append(['m/z', 'resolution', 'charge', 'intensity', 'MS2','scan_number','precursor_charge'])
+    ms2_list = []
+    
+    new = lst
+    
+    for i in new:
+        new_list.append(i.split())
+        if '@' in i:
+            x = i.split()
+            for y in x:
+                if '@' in y:
+                    ms2 = y[0:y.index('@')]
+                    ms2_list.append(str(ms2))
+    
+    header_list = new_list[0:26]
+    new_list = new_list[26:] # starts from line 26 to remove the first few header lines so that program could proceed
+    seperation_list = []
+    scan_number_list = []    
+    precursor_charge_list = []
+    rt_list = []
+    
+    for i in header_list:
+        if 'S' in i:
+            scan_number_list.append(i[1])
+        if 'Z' in i:
+            precursor_charge_list.append(i[1])
+        if 'RetTime' in i:
+            rt_list.append(i[2])
+    
+    for i in range(len(new_list)):
+        if 'RetTime' in new_list[i]:
+            seperation_list.append(i-1)
+        if 'PrecursorInt' in new_list[i]:
+            seperation_list.append(i+2)
+        if 'S' in new_list[i]:
+            scan_number_list.append(new_list[i][1])
+        if 'Z' in new_list[i]:
+            precursor_charge_list.append(new_list[i][1])
+        if 'RetTime' in new_list[i]:
+            rt_list.append(new_list[i][2])
+    
+    seperation_pairs = []
+    start = 0
+    for i in range(int(len(seperation_list)/2)):
+        seperation_pairs.append((seperation_list[i+start],seperation_list[i+start+1]))
+        start +=1 
+     
+    update_index = 0
+    for start,end in seperation_pairs:
+        start += update_index
+        end += update_index
+        new_list[start:end] = '-'
+        update_index -= (end-start-1)
+    
+    ms2_list_index = 0
+    scan_number_index = 0
+    precursor_charge_index = 0
+    rt_index = 0
+    
+    for element in new_list:
+        if element == '-':
+            ms2_list_index+=1
+            scan_number_index+=1
+            precursor_charge_index+=1
+            rt_index+=1
+            continue   
+        element.append(ms2_list[ms2_list_index])
+        element.append(scan_number_list[scan_number_index])
+        element.append(precursor_charge_list[precursor_charge_index])
+        element.append(rt_list[rt_index])
+        final_lst.append(element)
 
-#########Start formatting################
-with open(MS2_path) as input:
-    lst = [line.strip() for line in input] #appending each entry of the raw file to a list
-
-new_list= []
-final_lst = []
-final_lst.append(['m/z', 'resolution', 'charge', 'intensity', 'MS2','scan_number','precursor_charge','retention time'])
-ms2_list = []
-
-new = lst
-
-for i in new:
-    new_list.append(i.split())
-    if '@' in i:
-        x = i.split()
-        for y in x:
-            if '@' in y:
-                ms2 = y[0:y.index('@')]
-                ms2_list.append(str(ms2))
-
-header_list = new_list[0:26]
-new_list = new_list[26:] # starts from line 26 to remove the first few header lines so that program could proceed
-seperation_list = []
-RT_list = []
-scan_number_list = []    
-precursor_charge_list = []
-
-##This section is for extracting info from first scan
-
-for i in header_list:
-    if 'S' in i:
-        scan_number_list.append(i[1])
-    if 'Z' in i:
-        precursor_charge_list.append(i[1])
-    if 'RetTime' in i:
-        RT_list.append(i[2])
-
-##This section is for extracting info for the rest of the scans
-for i in range(len(new_list)):
-    if 'RetTime' in new_list[i]:
-        RT_list.append(new_list[i][2])
-    if 'PrecursorInt' in new_list[i]:
-        seperation_list.append(i+2)
-    if 'S' in new_list[i]:
-        scan_number_list.append(new_list[i][1])
-    if 'Z' in new_list[i]:
-        precursor_charge_list.append(new_list[i][1])
-
-seperation_pairs = []
-start = 0
-for i in range(int(len(seperation_list)/2)):
-    seperation_pairs.append((seperation_list[i+start],seperation_list[i+start+1]))
-    start +=1 
- 
-update_index = 0
-for start,end in seperation_pairs:
-    start += update_index
-    end += update_index
-    new_list[start:end] = '-'
-    update_index -= (end-start-1)
-
-ms2_list_index = 0
-scan_number_index = 0
-precursor_charge_index = 0
-ret_time_index = 0
-
-for element in new_list:
-    if element == '-':
-        ms2_list_index+=1
-        scan_number_index+=1
-        precursor_charge_index+=1
-        ret_time_index+=1
-        continue   
-    element.append(ms2_list[ms2_list_index])
-    element.append(scan_number_list[scan_number_index])
-    element.append(precursor_charge_list[precursor_charge_index])
-    element.append(RT_list[ret_time_index])
-    final_lst.append(element)
-
-final_list_formatted = []
-
-incorrect_entries = ['S','I','Z']
-
-for entry in final_lst:
-    if entry[0] not in incorrect_entries:
-        final_list_formatted.append(entry)
-
-out_name = output_directory + '\\'+tissue_type+'_formatted.txt'
-with open(out_name,'w') as output:
-    for i in final_list_formatted:
-        for j in i:
-            output.write(str(j + ','))
-        output.write('\n')
-
+    out_name = output_directory + '\\'+tissue_type+'_formatted_RT_old.txt'
+    with open(out_name,'w') as output:
+        for i in final_lst:
+            for j in i:
+                output.write(str(j + ','))
+            output.write('\n')
+    return out_name
+format_raw_MS2(MS2_path,output_directory)
